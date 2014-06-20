01c41650da9269a4c3ffaeeb76ccfc47ba10e432
Nate Fitch <nate.fitch@joyent.com>
2014-06-20 21:28:27 +0000

    MANTA-2197: Some people don't use external resolvers for Manta, LBs don't set up. MANTA-2107: Grepping out Binder google resolvers is a hack

--- boot/configure.sh
+++ boot/configure.sh
@@ -18,6 +18,31 @@ export PATH=$SVC_ROOT/build/node/bin:/opt/local/bin:/usr/sbin/:/usr/bin:$PATH
 MUPPET_CFG=$SVC_ROOT/etc/config.json
 
 
+function find_sdc_resolver {
+    local sapi_url=$(mdata-get SAPI_URL)
+    if [[ -z $sapi_url ]]; then
+        fatal "Unable to get SAPI_URL"
+    fi
+    local sapi_hostname=$(basename $sapi_url)
+    if [[ -z $sapi_hostname ]] || [[ $sapi_hostname != *sapi* ]]; then
+        fatal "$sapi_hostname isn't recognizable as sapi"
+    fi
+    SDC_RESOLVER=''
+    local resolvers=$(cat /etc/resolv.conf | grep nameserver | \
+        cut -d ' ' -f 2 | tr '\n' ' ')
+    for resolver in $resolvers; do
+        local sapi_ip=$(dig @$resolver $sapi_hostname +short)
+        if [[ -n "$sapi_ip" ]]; then
+            SDC_RESOLVER="$resolver"
+            break
+        fi
+    done
+    if [[ -z "$SDC_RESOLVER" ]]; then
+        fatal "No resolvers were able to resolve $sapi_hostname"
+    fi
+}
+
+
 function manta_setup_muppet {
     svccfg import /opt/smartdc/muppet/smf/manifests/muppet.xml
     svcadm enable muppet
@@ -58,17 +83,19 @@ function manta_setup_stud {
 # Mainline
 
 #
-# Since the external network is the primary network for this zone, the Google
-# DNS servers (8.8.8.8/8.8.4.4) will be first in the list of resolvers.  As this
-# zone is setting up, the config-agent can't resolve the SAPI hostname (e.g.
-# sapi.bh1-kvm6.joyent.us) and zone setup will fail.
+# Since the external network is the primary network for this zone, external DNS
+# servers will be first in the list of resolvers.  As this zone is setting up,
+# the config-agent can't resolve the SAPI hostname (e.g.  sapi.coal.joyent.us)
+# and zone setup will fail.
 #
-# Here, remove the external DNS servers so this zone's setup can finish
+# Here, remove all resolvers but the SDC resolver so setup can finish
 # appropriately.  As part of setup, the config-agent will rewrite the
-# /etc/resolv.conf file with the proper resolvers, so this hack just allows that
+# /etc/resolv.conf file with the proper resolvers, so this just allows that
 # agent to discover and download the appropriate zone configuration.
 #
-cat /etc/resolv.conf | grep -v 8.8.8.8 | grep -v 8.8.4.4 > /tmp/resolv.conf
+find_sdc_resolver
+cat /etc/resolv.conf | grep -v nameserver > /tmp/resolv.conf
+echo "nameserver $SDC_RESOLVER" >> /tmp/resolv.conf
 mv /tmp/resolv.conf /etc/resolv.conf
 
 echo "Running common setup scripts"
