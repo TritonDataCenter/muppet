1aa146271609c37a8748decd0172b7997ccfb7f3
Nate Fitch <nate.fitch@joyent.com>
2014-10-16 23:39:34 +0000

    MANTA-2479: Madtom and the Marlin dashboard should support external nic

--- boot/setup.sh
+++ boot/setup.sh
@@ -27,38 +27,6 @@ export PATH=$SVC_ROOT/build/node/bin:/opt/local/bin:/usr/sbin/:/usr/bin:$PATH
 MUPPET_CFG=$SVC_ROOT/etc/config.json
 
 
-function find_sdc_resolver {
-    local sapi_url=$(mdata-get SAPI_URL)
-    if [[ -z $sapi_url ]]; then
-        fatal "Unable to get SAPI_URL"
-    fi
-    local sapi_hostname=$(basename $sapi_url)
-    if [[ -z $sapi_hostname ]] || [[ $sapi_hostname != *sapi* ]]; then
-        fatal "$sapi_hostname isn't recognizable as sapi"
-    fi
-    SDC_RESOLVER=''
-    local resolvers=$(cat /etc/resolv.conf | grep nameserver | \
-        cut -d ' ' -f 2 | tr '\n' ' ')
-    for resolver in $resolvers; do
-        local sapi_ip;
-        sapi_ip=$(dig @$resolver $sapi_hostname +short)
-        if [[ $? != 0 ]]; then
-            echo "$resolver was unavailable to resolve $sapi_hostname"
-            continue
-        fi
-        if [[ -n "$sapi_ip" ]]; then
-            SDC_RESOLVER="$resolver"
-            break
-        else
-            echo "$resolver did not resolve $sapi_hostname"
-        fi
-    done
-    if [[ -z "$SDC_RESOLVER" ]]; then
-        fatal "No resolvers were able to resolve $sapi_hostname"
-    fi
-}
-
-
 function manta_setup_muppet {
     svccfg import /opt/smartdc/muppet/smf/manifests/muppet.xml
     svcadm enable muppet
@@ -98,22 +66,6 @@ function manta_setup_stud {
 
 
 # Mainline
-
-#
-# Since the external network is the primary network for this zone, external DNS
-# servers will be first in the list of resolvers.  As this zone is setting up,
-# the config-agent can't resolve the SAPI hostname (e.g.  sapi.coal.joyent.us)
-# and zone setup will fail.
-#
-# Here, remove all resolvers but the SDC resolver so setup can finish
-# appropriately.  As part of setup, the config-agent will rewrite the
-# /etc/resolv.conf file with the proper resolvers, so this just allows that
-# agent to discover and download the appropriate zone configuration.
-#
-find_sdc_resolver
-cat /etc/resolv.conf | grep -v nameserver > /tmp/resolv.conf
-echo "nameserver $SDC_RESOLVER" >> /tmp/resolv.conf
-mv /tmp/resolv.conf /etc/resolv.conf
 
 echo "Running common setup scripts"
 manta_common_presetup
