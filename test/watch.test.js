d958275a38deee63126e9ce111df361a8faad409
John Levon <john.levon@joyent.com>
2019-07-13 16:55:07 +0000

    m

---
+++ test/watch.test.js
@@ -0,0 +1,52 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright 2019 Joyent, Inc.
+ */
+var vasync = require('vasync');
+var helper = require('./helper.js');
+var watch = require('../lib/watch.js');
+
+var test = helper.test;
+var log = helper.createLogger();
+
+function MockZookeeper() {
+    this.res = {};
+}
+
+MockZookeeper.prototype.get = function (path, cb) {
+    cb(null, this.res[path]);
+};
+
+MockZookeeper.prototype.isConnected = function () {
+    return (true);
+};
+
+test('test FIXME', function (t) {
+    var zk = new MockZookeeper();
+    var watcher = new watch.ServerWatcherFSM({
+        zk: zk,
+        path: '',
+        log: log
+    });
+
+    watcher.on('serversChanged', function (servers) {
+        console.log(servers);
+        t.done();
+    });
+
+    zk.res['/c1'] = JSON.stringify({
+	type: 'host', host: { address: '127.0.0.1' } });
+    zk.res['/c2'] = JSON.stringify({
+	type: 'host', host: { address: '127.0.0.2' } });
+
+    console.log('here');
+    watcher.childrenChanged(['c1', 'c2']);
+
+});
+
+// vim: set softtabstop=4 shiftwidth=4:
